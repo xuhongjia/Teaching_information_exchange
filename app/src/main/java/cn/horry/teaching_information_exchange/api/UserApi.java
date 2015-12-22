501572f6e5914628c6b6f4d68b9c4fa3c80c156d
xuhongjia <xhj13724194657>
2015-12-22 21:50:11 +0800

    注册功能

--- app/src/main/java/cn/horry/teaching_information_exchange/api/UserApi.java
+++ app/src/main/java/cn/horry/teaching_information_exchange/api/UserApi.java
@@ -3,6 +3,7 @@ package cn.horry.teaching_information_exchange.api;
 import org.kymjs.kjframe.http.HttpCallBack;
 import org.kymjs.kjframe.http.Request;
 
+import cn.horry.teaching_information_exchange.entity.User;
 import cn.horry.teaching_information_exchange.utils.MyHttpParams;
 
 /**
@@ -17,6 +18,11 @@ public class UserApi extends API{
     public static void getCourse(int uId , int isTeacher ,int page, HttpCallBack httpCallBack){
         String url = URL.concat("action/user/getCourse");
         MyHttpParams params = new MyHttpParams("uId",uId,"isTeacher",isTeacher,"page",page);
+        builderHttp.url(url).httpMethod(Request.HttpMethod.POST).params(params).callback(httpCallBack).useCache(true).request();
+    }
+    public static void register(User user,HttpCallBack httpCallBack){
+        String url = URL.concat("action/user/register");
+        MyHttpParams params = new MyHttpParams(user);
         builderHttp.url(url).httpMethod(Request.HttpMethod.POST).params(params).callback(httpCallBack).useCache(true).request();
     }
 }
--- app/src/main/java/cn/horry/teaching_information_exchange/ui/activity/RegisterActivity.java
+++ app/src/main/java/cn/horry/teaching_information_exchange/ui/activity/RegisterActivity.java
@@ -1,5 +1,10 @@
 package cn.horry.teaching_information_exchange.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -8,12 +13,21 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.kymjs.kjframe.http.HttpCallBack;
 import org.kymjs.kjframe.ui.AnnotateUtil;
 import org.kymjs.kjframe.ui.BindView;
 
 import cn.horry.teaching_information_exchange.R;
+import cn.horry.teaching_information_exchange.api.API;
+import cn.horry.teaching_information_exchange.api.UserApi;
+import cn.horry.teaching_information_exchange.entity.GeneralResponse;
+import cn.horry.teaching_information_exchange.entity.User;
 import cn.horry.teaching_information_exchange.listener.MySmsListener;
 import cn.horry.teaching_information_exchange.utils.MobSMS;
 import cn.horry.teaching_information_exchange.utils.TimeCount;
@@ -32,6 +46,14 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     private TextView left;
     @BindView(id = R.id.title , click = true)
     private TextView title;
+    @BindView(id = R.id.register_progress)
+    private View mProgressView;
+    @BindView(id = R.id.password)
+    private TextView password;
+    @BindView(id = R.id.register_form)
+    private View register_form;
+    @BindView(id = R.id.isTeacher)
+    private RadioGroup isTeacher;
     private TimeCount count;
     private MobSMS mobSMS;
     private Handler handler;
@@ -75,10 +97,49 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     }
 
     public void PassVerification(){
-        showShortText("注册成功，请等待！");
-        finish();
+        User user = new User();
+        user.setAccount(account.getText().toString().trim());
+        user.setPassword(password.getText().toString().trim());
+        user.setImg(API.DEFAULT_HEAD_IMG);
+        user.setName("用户" + String.valueOf(System.currentTimeMillis()).substring(6));
+        user.setLastLoginTime(System.currentTimeMillis());
+        if(isTeacher.getCheckedRadioButtonId()==R.id.teacher)
+        {
+            user.setIsTeacher(1);
+        }else
+        {
+            user.setIsTeacher(0);
+        }
+        user.setAge(0);
+        user.setSex(0);
+        UserApi.register(user, new HttpCallBack() {
+            @Override
+            public void onFailure(int errorNo, String strMsg) {
+                super.onFailure(errorNo, strMsg);
+
+            }
+
+            @Override
+            public void onSuccess(String t) {
+                super.onSuccess(t);
+                GeneralResponse<String> response = new Gson().fromJson(t, new TypeToken<GeneralResponse<String>>() {
+                }.getType());
+                if (response.isSuccess()) {
+                    showProgress(false);
+                    showShortText("注册成功！");
+                    gotoMain();
+                } else {
+                    showProgress(false);
+                    showShortText(response.getMsg());
+                }
+            }
+        });
     }
 
+    /**
+     * 点击事件
+     * @param v 点击的View
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId())
@@ -88,7 +149,10 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
                 mobSMS.getSmssdk().getVerificationCode("86",account.getText().toString().trim());
                 break;
             case R.id.submit_validation :
-                mobSMS.getSmssdk().submitVerificationCode("86",account.getText().toString().trim(),validation_code.getText().toString().trim());
+                if(checkValue()) {
+                    showProgress(true);
+                    mobSMS.getSmssdk().submitVerificationCode("86", account.getText().toString().trim(), validation_code.getText().toString().trim());
+                }
                 break;
             case R.id.left:
                 finish();
@@ -96,5 +160,66 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
             default:
                 break;
         }
+    }
+    /**
+     *判断输入框的值是否合法
+     */
+    private boolean checkValue(){
+        if(account.getText().toString().trim().length()!=11)
+        {
+            showShortText("请输入正确的手机号码！");
+            return false;
+        }
+        if(password.getText().toString().trim().length()<6)
+        {
+            showShortText("密码需要大于等于6位！");
+            return false;
+        }
+        if(validation_code.getText().toString().trim().length()!=4)
+        {
+            showShortText("请输入4位验证码！");
+        }
+        return true;
+    }
+
+    /**
+     * 判断显示progress
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // 大于等于sdk 13的
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            register_form.setVisibility(show ? View.GONE : View.VISIBLE);
+            register_form.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    register_form.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            //不同sdk的设置
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            register_form.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+    /**
+     * 跳转到主页
+     */
+    private void gotoMain(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
