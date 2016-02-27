96e37ec871bbb4deb2b94c8415cd0cebf7f0ee33
xuhongjia <xhj13724194657>
2016-02-27 22:51:52 +0800

    功能完善

--- app/src/main/java/cn/horry/teaching_information_exchange/api/API.java
+++ app/src/main/java/cn/horry/teaching_information_exchange/api/API.java
@@ -9,6 +9,6 @@ public abstract class API {
     public static String REAL_URL = "http://horry.me:8080/TeachingInformationExchangeWeb/";
     public static String TEST_URL = "http://192.168.1.145:8080/web/";
     public static String DEFAULT_HEAD_IMG = "http://horry.oss-cn-shenzhen.aliyuncs.com/43d19356-cd11-4a5e-b5aa-e87cd974823a.png";
-    public static String URL = TEST_URL;
+    public static String URL = REAL_URL;
     public static KJHttp.Builder builderHttp = new KJHttp.Builder();
 }
